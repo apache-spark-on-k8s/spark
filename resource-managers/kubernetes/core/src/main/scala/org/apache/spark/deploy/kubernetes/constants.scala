6699072562257797eb8b16b76e7fc99b09402476
mcheah <mcheah@palantir.com>
2017-02-02 12:24:55 -0800

    Add delay to liveliness check

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/kubernetes/constants.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/kubernetes/constants.scala
@@ -67,4 +67,5 @@ package object constants {
   // Miscellaneous
   private[spark] val DRIVER_CONTAINER_NAME = "spark-kubernetes-driver"
   private[spark] val KUBERNETES_SUBMIT_SSL_NAMESPACE = "kubernetes.submit"
+  private[spark] val DRIVER_INITIAL_LIVELINESS_CHECK_DELAY_SECONDS = 10
 }
