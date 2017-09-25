0b9210c9408b631760c7017e115984aa8d50a291
Ilan Filonenko <if56@cornell.edu>
2017-09-25 15:19:44 -0400

    case sensitive file names in unix

--- resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesSuite.scala
+++ resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesSuite.scala
@@ -466,9 +466,9 @@ private[spark] object KubernetesSuite {
   val PYSPARK_SORT_CONTAINER_LOCAL_FILE_LOCATION =
     "local:///opt/spark/examples/src/main/python/sort.py"
   val SPARK_R_DATAFRAME_SUBMITTER_FILE_LOCATION =
-    "local:///opt/spark/examples/src/main/r/dataframe.R"
+    "local:///opt/spark/examples/src/main/R/dataframe.R"
   val SPARK_R_DATAFRAME_CONTAINER_LOCAL_FILE_LOCATION =
-    "src/test/r/dataframe.R"
+    "src/test/R/dataframe.R"
   val PYSPARK_PI_SUBMITTER_LOCAL_FILE_LOCATION = "src/test/python/pi.py"
   val FILE_EXISTENCE_MAIN_CLASS = "org.apache.spark.deploy.k8s" +
     ".integrationtest.jobs.FileExistenceTest"
