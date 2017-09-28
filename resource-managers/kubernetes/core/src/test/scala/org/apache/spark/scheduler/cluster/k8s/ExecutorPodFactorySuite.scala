47ece8763386daee051124bc9e5e070a7161436a
Erik Erlandson <eerlands@redhat.com>
2017-09-28 14:39:31 -0700

    fix package clause to k8s

--- resource-managers/kubernetes/core/src/test/scala/org/apache/spark/scheduler/cluster/k8s/ExecutorPodFactorySuite.scala
+++ resource-managers/kubernetes/core/src/test/scala/org/apache/spark/scheduler/cluster/k8s/ExecutorPodFactorySuite.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.spark.scheduler.cluster.kubernetes
+package org.apache.spark.scheduler.cluster.k8s
 
 import scala.collection.JavaConverters._
 
